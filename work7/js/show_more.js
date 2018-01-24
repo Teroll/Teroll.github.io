5743f591ea2f874658e8b288640409acfc128f24
Sergey <rexarko@mail.ru>
2018-01-24 16:22:37 +0500

    П7-Д5
    Страница продукта готова. Добавлены ссылки на скачивание и на заказ

--- work7/js/show_more.js
+++
@@ -1,5 +0,0 @@
- $(function(){
-            $('.show_more').click(function(){
-                $('.page2').toggleClass('.show_page');
-            });
-        });
---
+++ work7/js/show_more_mobile.js
@@ -0,0 +1,8 @@
+        jQuery(document).ready(function(){
+
+        	$(function() {
+        		$("#show_more_mobile").click(function() {
+        			$(".page2_mobile").toggleClass("show_page");
+        		})
+        	});
+        });
