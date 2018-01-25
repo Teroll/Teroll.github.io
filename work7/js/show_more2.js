909e75eef7aefac0796f78c209d67b80a98f8d71
Sergey <rexarko@mail.ru>
2018-01-25 12:44:21 +0500

    П7-Д6
    Исправлено:
    Падинги текстового бокса
    Радиуса
    Падинг между страницами
    Переход кнопки показать еще на новое место

--- work7/js/show_more2.js
+++ work7/js/show_more2.js
@@ -3,6 +3,8 @@
         	$(function() {
         		$("#show_more").click(function() {
         			$("#page2").toggleClass("show_page");
+        			$("#show_more").toggleClass("hide_page");
+        			$("#Hidden_block").toggleClass("show_page");
         		})
         	});
         });
