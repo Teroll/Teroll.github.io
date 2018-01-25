44d4697af0a8b278c786803e877f63a6378cf93d
Sergey <rexarko@mail.ru>
2018-01-25 20:25:24 +0500

    П7-Д6
    Добавлены теги

--- work7/js/show_more_mobile.js
+++ work7/js/show_more_mobile.js
@@ -3,6 +3,8 @@
         	$(function() {
         		$("#show_more_mobile").click(function() {
         			$(".page2_mobile").toggleClass("show_page");
+        			$("#show_more_mobile").toggleClass("hide_page");
+        			$("#show_more_mobile_2").toggleClass("show_page");
         		})
         	});
         });
