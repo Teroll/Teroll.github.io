87960b8600936b716ffb6810a6b797c8b5eed414
Sergey <rexarko@mail.ru>
2018-01-29 15:26:23 +0500

    Исправления готовы

--- work7/js/fixed_nav_after_scroll.js
+++ work7/js/fixed_nav_after_scroll.js
@@ -4,13 +4,35 @@ $(document).scroll(function() {
     $('#navbar').addClass('fixedattop');
     // $('#navbar').addClass('logo_mobile_compact');
     var shiftContent = $('#navbar').height();
-    $('#content').css('margin-top', shiftContent + 'px');
+    $('#mobile_content').css('margin-top', shiftContent + 'px');
   }
   else if ($('#navbar').hasClass('fixedattop')) {
     $('#navbar').removeClass('fixedattop');
     $('#navbar').removeClass('logo_mobile_compact');
     $('#logo_button').removeClass('logo_mobile_open');
     $('#logo').removeClass('show');
-    $('#content').css('margin-top', '0px');
+    $('#mobile_content').css('margin-top', '0px');
   }
 });
+
+// $(window).resize(function() {
+//   width = $(window).width();
+//   if (width <= 600) {
+//    $(document).scroll(function() {
+//     var documentScrollTop = $(document).scrollTop();
+//     if (documentScrollTop > $('#header').height()) {
+//       $('#navbar').addClass('fixedattop');
+//     // $('#navbar').addClass('logo_mobile_compact');
+//     var shiftContent = $('#navbar').height();
+//     $('#mobile_content').css('margin-top', shiftContent + 'px');
+//   }
+//   else if ($('#navbar').hasClass('fixedattop')) {
+//     $('#navbar').removeClass('fixedattop');
+//     $('#navbar').removeClass('logo_mobile_compact');
+//     $('#logo_button').removeClass('logo_mobile_open');
+//     $('#logo').removeClass('show');
+//     $('#mobile_content').css('margin-top', '0px');
+//   }
+// });
+//  }
+// });
---
+++ work7/js/mobile_script_on.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+{
+if (screen.width < 500) document.write ('<script type="text/javascript" src="fixed_nav_after_scroll.js" ></sc' + 'ript>');
+}
+</script>
