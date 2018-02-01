e814b54a1fa2f4aa20547a738331131eb14b01de
Sergey <rexarko@mail.ru>
2018-02-01 14:02:10 +0500

    скролл сохранение готово

--- work7/js/save_scroll.js
+++ work7/js/save_scroll.js
@@ -1,9 +1,11 @@
-var $window = $(window)
+$(document).ready(function() { 
+	var $window = $(window)
 
-/* Restore scroll position */
-window.scroll(0, localStorage.getItem('scrollPosition')|0)
+	/* Restore scroll position */
+	window.scroll(0, localStorage.getItem('scrollPosition')|0)
 
-/* Save scroll position */
-$window.scroll(function () {
-    localStorage.setItem('scrollPosition', $window.scrollTop())
-})
+	/* Save scroll position */
+	$window.scroll(function () {
+		localStorage.setItem('scrollPosition', $window.scrollTop())
+	})
+});
