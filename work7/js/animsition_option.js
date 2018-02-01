a531524dfc47f837c481187b84ec3dba082719f1
Sergey <rexarko@mail.ru>
2018-02-01 13:00:43 +0500

    испр3

--- work7/js/animsition_option.js
+++ work7/js/animsition_option.js
@@ -10,9 +10,9 @@ $(document).ready(function() {
     loadingParentElement: 'body', //animsition wrapper element
     loadingClass: 'animsition-loading',
     loadingInner: '', // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 2000,
-    onLoadEvent: false,
+    timeout: true,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
     browser: [ 'animation-duration', '-webkit-animation-duration'],
     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
