b06a7e148c9e91b85dfb2af0c5f6d5c69616cc1f
Sergey <rexarko@mail.ru>
2018-02-01 14:48:47 +0500

    форма

---
+++ work7/js/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "rexarko@mail.ru";
+$sitename = "I`am";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nТелефон/почта: $phone \nТекст: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
