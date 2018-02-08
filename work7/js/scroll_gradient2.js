d07adc1a500b9e2c98961e9519ab821db7d5c9a7
Sergey <rexarko@mail.ru>
2018-02-08 10:56:22 +0500

    имена

--- work7/js/scroll_gradient2.js
+++ work7/js/scroll_gradient2.js
@@ -1,15 +1,24 @@
 // $('#target').scroll(function(){
-//   var elem = $('#target');
-//         if (elem.scrollTop() - $('#target').height() > 101) {
-//             $('.shadow').addClass('shadow2');
+//         if ($('#target').scrollTop() = $('#target')[0].scrollHeight) {
+//             $('.shadow').removeClass('shadow2');
 //         } 
 //         else {
-//             $('.shadow').removeClass('shadow2');
+           
+//              $('.shadow').addClass('shadow2');
 //         }
 //     })
 
-// $('#target').animate({scrollTop: $('#target')[0].scrollHeight});
-// var elem = $('#target');
-// if (elem[0].scrollHeight - elem.scrollTop() == elem.outerHeight()) {
-//   // We're at the bottom.
-// }
+// console.log("bottom");
+
+// $('#target').on("scroll", function() {
+// 	var scrollHeight = $(document).height();
+// 	var scrollPosition = $('#target').height() + $(window).scrollTop();
+// 	if ((scrollHeight - scrollPosition)  < 30) {
+// 	    // when scroll to bottom of the page
+// 	    $('.shadow').removeClass('shadow2');
+// 	}
+// 	else {
+//             $('.shadow').addClass('shadow2');
+//         }
+// });
+// $('#target').scrollTop() = $('#target')[0].scrollHeight;
