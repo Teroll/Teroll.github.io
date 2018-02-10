a144c7467835360fca132ca771578ce885d2c36d
Sergey <rexarko@mail.ru>
2018-02-10 13:38:04 +0500

    д4

--- work8/js/fixed_nav_after_scroll.js
+++ work8/js/fixed_nav_after_scroll.js
@@ -4,12 +4,12 @@ jQuery(document).ready(function(){
     $(document).scroll(function() {
       var documentScrollTop = $(document).scrollTop();
       if (documentScrollTop > $('#header').height()) {
-        $('#navbar').addClass('fixedattop');
-        var shiftContent = $('#navbar').height();
+        $('.menu_laptop').addClass('fixedattop');
+        var shiftContent = $('.menu_laptop').height();
         $('#mobile_content').css('margin-top', shiftContent + 'px');
       }
-      else if ($('#navbar').hasClass('fixedattop')) {
-        $('#navbar').removeClass('fixedattop');
+      else if ($('.menu_laptop').hasClass('fixedattop')) {
+        $('.menu_laptop').removeClass('fixedattop');
       }
     });
 
