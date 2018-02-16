d85f6ea5febd5919d8902f71d3e1b619c09ae67d
Sergey <rexarko@mail.ru>
2018-02-16 19:15:48 +0500

    анимация гот

--- work6/js/body_off.js
+++ work6/js/body_off.js
@@ -3,7 +3,16 @@ jQuery(document).ready(function(){
 	$(function() {
 		$(".box").click(function(event) {
 			
-			$(this).css({"transform": "rotateY(360deg)", "transition": "transform 2.2s linear"});
+			// $(this).css({
+			// 	"transform": "rotateY(359deg)",
+			// 	"transition": "transform 0.3s linear"
+			// 	// @keyframes spinHorizontal {
+			// 	// 	0% { transform: rotateY(0deg); }
+			// 	// 	100% { transform: rotateY(360deg); }
+			// 	// }
+			// });
+			
+			$(this).addClass("imageRotateHorizontal");
 			$(this).removeClass("body");
 		})
 	});
