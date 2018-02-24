c1f6ca685735bc0de85749ee4bd4ee6a28bb75f4
Sergey <rexarko@mail.ru>
2018-02-24 12:53:23 +0500

    1

---
+++ work5/js/dots_scroll.js
@@ -0,0 +1,15 @@
+$('.dot-header').click(function(){
+       $('html, body').animate({scrollTop:$('#header').position().top}, 1700);
+});
+$('.dot-Our_advantage').click(function(){
+       $('html, body').animate({scrollTop:$('#Our_advantage').position().top}, 1700);
+});
+$('.dot-about_us').click(function(){
+       $('html, body').animate({scrollTop:$('#about_us').position().top}, 1700);
+});
+$('.dot-how_we_work').click(function(){
+       $('html, body').animate({scrollTop:$('#how_we_work').position().top}, 1700);
+});
+$('.dot-book').click(function(){
+       $('html, body').animate({scrollTop:$('#book').position().top}, 1700);
+});
